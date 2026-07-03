9b23fc1f9df3dfedd8557c80e5aca155b178ba09
joydebtopupbd <joydeb23n@gmail.com>
2026-07-04 00:14:02 +0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8WTR_OuyUbfIFe7F0tWc7PygwgSl3SVE",
+  authDomain: "joydeb-topup-bd.firebaseapp.com",
+  projectId: "joydeb-topup-bd",
+  storageBucket: "joydeb-topup-bd.firebasestorage.app",
+  messagingSenderId: "295962899771",
+  appId: "1:295962899771:web:b12c374a7d8b8f406e331e"
+};
+
+export const app = initializeApp(firebaseConfig);
