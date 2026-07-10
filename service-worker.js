f8ce1af380ebbfdaba26ddbd297d8260fe4ef2db
joydebtopupbd <joydeb23n@gmail.com>
2026-07-10 13:01:19 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,3 +9,35 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", (event) => {
   event.respondWith(fetch(event.request));
 });
+const CACHE_NAME = "joydeb-topup-v1";
+
+const urlsToCache = [
+  "/joydeb-topup-bd/",
+  "/joydeb-topup-bd/index.html",
+  "/joydeb-topup-bd/style.css",
+  "/joydeb-topup-bd/script.js",
+  "/joydeb-topup-bd/manifest.json",
+  "/joydeb-topup-bd/icon-192.png",
+  "/joydeb-topup-bd/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
